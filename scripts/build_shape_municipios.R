ed2d01d2b5706630f346f897547e6da5f972ad61
rebecacarvalho <rebeca.jesus.carvalho@usp.br>
2020-04-06 12:25:22 -0300

    reduz o tamanho dos shapes

--- scripts/build_shape_municipios.R
+++ scripts/build_shape_municipios.R
@@ -1,57 +1,63 @@
 rm(list = ls())
 
 library(tidyverse)
-library(sf)
-library(RCurl)
+library(geobr)
+
 
 # 1. Download Shape -------------------------------------------------------
 
+estados <- c("AC", "AL", "AM", 
+             "AP", "BA", "CE", "DF", "ES","GO",
+             "MA", "MG","MS", "MT", "PA", "PB", 
+             "PE", "PI","PR", "RJ", "RN", "RO", 
+             "RR","RS", "SC", "SE", "SP", "TO")
 
-temp_file <- tempfile()
 
-temp_dir <- tempdir()
+for(i in estados){
+  
+  cat("Lendo", i, "\n")
 
-url_use <- "ftp://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2018/Brasil/BR/br_municipios.zip"
+  mun <- read_municipality(code_muni= i, 
+                         year=2018,
+                         simplified = TRUE)
 
-download.file(url_use, destfile = temp_file)
+  mun <- mun %>% 
+    dplyr::select(code_muni,
+                name_muni,
+                abbrev_state,
+                geom) %>% 
+    dplyr::rename("COD_MUN_IBGE" = "code_muni",
+                "NOME_MUNICIPIO" = "name_muni",
+                "UF" = "abbrev_state",
+                "geometry" = "geom")
 
-unzip(temp_file, exdir = temp_dir)
+  for(i in seq_along(mun$COD_MUN_IBGE)){
+    mun_shape_use <- mun[mun$COD_MUN_IBGE == mun$COD_MUN_IBGE[[i]],]
+    readr::write_rds(mun_shape_use, paste0("data/output/shape_municipios/", 
+                                         mun$COD_MUN_IBGE[[i]],".rds"))
+    }
 
-mun_shape <- sf::read_sf("br_municipios/BRMUE250GC_SIR.shp")
+  }
 
-mun_shape <- mun_shape %>% 
-  dplyr::rename("NOME_MUNICIPIO" = "NM_MUNICIP",
-                "COD_MUN_IBGE" = "CD_GEOCMU")
 
-mun_shape$NOME_MUNICIPIO <- stringr::str_to_title(mun_shape$NOME_MUNICIPIO)
-
-getwd()
-
-# 2. Save -----------------------------------------------------------------
-
-for(i in seq_along(mun_shape$COD_MUN_IBGE)){
-  mun_shape_use <- mun_shape[mun_shape$COD_MUN_IBGE == mun_shape$COD_MUN_IBGE[[i]],]
-  readr::write_rds(mun_shape_use, paste0("data/output/shape_municipios/", mun_shape$COD_MUN_IBGE[[i]],".rds"))
-}
 
 length(list.files("data/output/shape_municipios/"))
 
-# 3. Shape Brasil ---------------------------------------------------------
-
-temp_file <- tempfile()
-
-temp_dir <- tempdir()
-
-url_use = "ftp://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2015/Brasil/BR/br_unidades_da_federacao.zip"
-
-download.file(url_use, destfile = temp_file)
-
-unzip(temp_file, exdir = temp_dir)
-
-br_shape <- sf::read_sf(paste0(temp_dir,"/BRUFE250GC_SIR.shp"))
-
-readr::write_rds(br_shape, paste0("data/output/shape_municipios/br.rds"))
+# 2. Shape Brasil ---------------------------------------------------------
 
 
+br <- read_state(code_state = "all",
+                 year = 2018)
+
+br <- br %>% 
+  dplyr::select(abbrev_state,
+                geom) %>% 
+  dplyr::rename("UF" = "abbrev_state",
+                "geometry" = "geom")
+
+readr::write_rds(br, paste0("data/output/shape_municipios/br.rds"))
+
+
+length(list.files("data/output/shape_municipios/"))
 
 
